c1dff8bec5574e09825d3abafa1b4dba1fa40b22
ecrofeg <ecrofeg1@gmail.com>
2019-01-23 22:25:22 +0400

    Prettier

--- server/index.js
+++ server/index.js
@@ -1,44 +1,44 @@
-var app = require("express")();
-var http = require("http").Server(app);
+require('dotenv').config();
 
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/telesram");
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log('MongoDB is connected'));
 
-const Message = mongoose.model("Message", {
-  author: String,
-  text: String,
-  color: String,
-  date: Date
+const Message = mongoose.model(
+	'Message',
+	new mongoose.Schema({
+		author: String,
+		text: String,
+		color: String,
+		date: Date
+	})
+);
+
+const loadMessages = () => Message.find({}).exec((error, res) => io.emit('load messages', res));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-const loadMessages = socket => {
-  Message.find({}).exec((error, res) => socket.emit("load messages", res));
-};
+io.on('connection', socket => {
+	loadMessages();
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+	socket.on('add message', newMessage => {
+		const message = new Message({
+			author: newMessage.author,
+			text: newMessage.text,
+			color: newMessage.color,
+			date: new Date()
+		});
 
-io.on("connection", socket => {
-  loadMessages(socket);
-
-  socket.on("add message", newMessage => {
-    const message = new Message({
-      author: newMessage.author,
-      text: newMessage.text,
-      color: newMessage.color,
-      date: new Date()
-    });
-
-    message.save(() => loadMessages(socket));
-  });
+		message.save().then(() => loadMessages());
+	});
 });
 
 http.listen(7000, function() {
-  console.log("listening on *:7000");
+	console.log('listening on *:7000');
 });
--- src/App.js
+++ src/App.js
@@ -1,235 +1,227 @@
-import React, { Component } from "react";
-import cn from "classnames";
-import io from "socket.io-client";
-import { format } from "date-fns";
-import "./App.css";
+import React, { Component } from 'react';
+import cn from 'classnames';
+import io from 'socket.io-client';
+import { format } from 'date-fns';
+import './App.css';
 
-const USER_KEY = "bochka-user-key";
+const USER_KEY = 'bochka-user-key';
 
 const colors = [
-  "#f44336",
-  "#e91e63",
-  "#9c27b0",
-  "#673ab7",
-  "#3f51b5",
-  "#2196f3",
-  "#03a9f4",
-  "#00bcd4",
-  "#009688",
-  "#4caf50",
-  "#8bc34a",
-  "#cddc39",
-  "#ffeb3b",
-  "#ffc107",
-  "#ff9800",
-  "#ff5722",
-  "#795548"
+	'#f44336',
+	'#e91e63',
+	'#9c27b0',
+	'#673ab7',
+	'#3f51b5',
+	'#2196f3',
+	'#03a9f4',
+	'#00bcd4',
+	'#009688',
+	'#4caf50',
+	'#8bc34a',
+	'#cddc39',
+	'#ffeb3b',
+	'#ffc107',
+	'#ff9800',
+	'#ff5722',
+	'#795548'
 ];
 
 class App extends Component {
-  socket = null;
+	socket = null;
 
-  state = {
-    messages: [],
-    inputText: "",
-    authorInputText: "",
-    user: "",
-    authorColor: "#ffffff"
-  };
+	state = {
+		messages: [],
+		inputText: '',
+		authorInputText: '',
+		user: '',
+		authorColor: '#ffffff'
+	};
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onAuth = this.onAuth.bind(this);
-    this.onAuthInputChange = this.onAuthInputChange.bind(this);
-    this.onEnterKeyPressed = this.onEnterKeyPressed.bind(this);
-    this.onNewMessageButtonClick = this.onNewMessageButtonClick.bind(this);
-  }
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onAuth = this.onAuth.bind(this);
+		this.onAuthInputChange = this.onAuthInputChange.bind(this);
+		this.onEnterKeyPressed = this.onEnterKeyPressed.bind(this);
+		this.onNewMessageButtonClick = this.onNewMessageButtonClick.bind(this);
+	}
 
-  onAuth() {
-    if (this.state.authorInputText && this.state.authorInputText.trim()) {
-      localStorage.setItem(
-        USER_KEY,
-        JSON.stringify({
-          name: this.state.authorInputText,
-          color: colors[Math.floor(Math.random() * colors.length)]
-        })
-      );
+	onAuth() {
+		if (this.state.authorInputText && this.state.authorInputText.trim()) {
+			localStorage.setItem(
+				USER_KEY,
+				JSON.stringify({
+					name: this.state.authorInputText,
+					color: colors[Math.floor(Math.random() * colors.length)]
+				})
+			);
 
-      setTimeout(() => this.load(), 100);
-    } else {
-      alert("SAY ME YOUR FUCKING NAME");
-    }
-  }
+			setTimeout(() => this.load(), 100);
+		} else {
+			alert('SAY ME YOUR FUCKING NAME');
+		}
+	}
 
-  onAuthInputChange(event) {
-    this.setState({
-      authorInputText: event.target.value
-    });
-  }
+	onAuthInputChange(event) {
+		this.setState({
+			authorInputText: event.target.value
+		});
+	}
 
-  load() {
-    let userInfo = localStorage.getItem(USER_KEY);
+	load() {
+		let userInfo = localStorage.getItem(USER_KEY);
 
-    if (userInfo) {
-      userInfo = JSON.parse(userInfo);
+		if (userInfo) {
+			userInfo = JSON.parse(userInfo);
 
-      this.setState({
-        user: userInfo.name,
-        authorColor: userInfo.color
-      });
+			this.setState({
+				user: userInfo.name,
+				authorColor: userInfo.color
+			});
 
-      this.socket = io("http://95.84.25.99:7000/");
+			this.socket = io('http://95.84.25.99:7000/');
 
-      this.socket.on("load messages", result => {
-        this.setState({
-          messages: result
-        });
+			this.socket.on('load messages', result => {
+				this.setState({
+					messages: result
+				});
 
-        window.scrollTo(0, document.body.scrollHeight);
-      });
-    }
-  }
+				setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 200);
+			});
+		}
+	}
 
-  componentDidMount() {
-    this.load();
-    window.scrollTo(0, document.body.scrollHeight);
-  }
+	componentDidMount() {
+		this.load();
 
-  onInputChange(event) {
-    this.setState({
-      inputText: event.target.value
-    });
-  }
+		setTimeout(
+			() => document.getElementById('newMessageInput') && document.getElementById('newMessageInput').focus(),
+			200
+		);
+	}
 
-  sendMessage() {
-    if (this.state.inputText && this.state.inputText.trim()) {
-      this.socket.emit("add message", {
-        text: this.state.inputText,
-        author: this.state.user,
-        color: this.state.authorColor
-      });
+	onInputChange(event) {
+		this.setState({
+			inputText: event.target.value
+		});
+	}
 
-      this.setState({
-        inputText: ""
-      });
-    }
-  }
+	sendMessage() {
+		if (this.state.inputText && this.state.inputText.trim()) {
+			this.socket.emit('add message', {
+				text: this.state.inputText,
+				author: this.state.user,
+				color: this.state.authorColor
+			});
 
-  onNewMessageButtonClick() {
-    this.sendMessage();
+			this.setState({
+				inputText: ''
+			});
 
-    setTimeout(() => document.getElementById("newMessageInput").focus(), 50);
-  }
+			setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 100);
+		}
+	}
 
-  onEnterKeyPressed(event) {
-    if (event.key === "Enter" && !event.shiftKey) {
-      this.sendMessage();
+	onNewMessageButtonClick() {
+		this.sendMessage();
 
-      event.preventDefault();
-    }
-  }
+		setTimeout(() => document.getElementById('newMessageInput').focus(), 50);
+	}
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.user ? (
-          <>
-            <div className="messages">
-              {this.state.messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={cn("message", {
-                    message_smallMargin:
-                      this.state.messages[index + 1] &&
-                      this.state.messages[index + 1].author === message.author
-                  })}
-                >
-                  {(!this.state.messages[index - 1] ||
-                    this.state.messages[index - 1].author !==
-                      message.author) && (
-                    <div
-                      className="message-author"
-                      style={{ color: message.color }}
-                    >
-                      {message.author},&nbsp;
-                      <span className="message-date">
-                        {format(message.date, "HH:mm")}
-                      </span>
-                    </div>
-                  )}
+	onEnterKeyPressed(event) {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			this.sendMessage();
 
-                  <span
-                    className={cn("message-text", {
-                      "message-text_yours": this.state.user === message.author
-                    })}
-                  >
-                    {message.text.split("\n").map(function(item, key) {
-                      return (
-                        <span key={key}>
-                          {item}
-                          <br />
-                        </span>
-                      );
-                    })}
-                  </span>
-                </div>
-              ))}
-            </div>
+			event.preventDefault();
+		}
+	}
 
-            <div className="newMessage">
-              <textarea
-                id="newMessageInput"
-                className="newMessage-input"
-                value={this.state.inputText}
-                onChange={this.onInputChange}
-                onKeyPress={this.onEnterKeyPressed}
-                autoComplete="off"
-                spellCheck="false"
-                placeholder="Bochka is listening..."
-                required={true}
-              />
+	render() {
+		return (
+			<div className="app">
+				{this.state.user ? (
+					<>
+						<div className="messages">
+							{this.state.messages.map((message, index) => (
+								<div
+									key={index}
+									className={cn('message', {
+										message_smallMargin:
+											this.state.messages[index + 1] &&
+											this.state.messages[index + 1].author === message.author
+									})}
+								>
+									{(!this.state.messages[index - 1] ||
+										this.state.messages[index - 1].author !== message.author) && (
+										<div className="message-author" style={{ color: message.color }}>
+											{message.author},&nbsp;
+											<span className="message-date">{format(message.date, 'HH:mm')}</span>
+										</div>
+									)}
 
-              <div
-                className="newMessage-button"
-                onClick={this.onNewMessageButtonClick}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
-                  <path d="M0 0h24v24H0z" fill="none" />
-                </svg>
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className="auth">
-            <h1>WHO THE FUCK ARE YOU?</h1>
+									<span
+										className={cn('message-text', {
+											'message-text_yours': this.state.user === message.author
+										})}
+									>
+										{message.text.split('\n').map(function(item, key) {
+											return (
+												<span key={key}>
+													{item}
+													<br />
+												</span>
+											);
+										})}
+									</span>
+								</div>
+							))}
+						</div>
 
-            <input
-              className="auth-input"
-              placeholder="I am..."
-              autoComplete="off"
-              autoCorrect="off"
-              spellCheck="false"
-              value={this.state.authorInputText}
-              onChange={this.onAuthInputChange}
-              required={true}
-            />
+						<div className="newMessage">
+							<textarea
+								id="newMessageInput"
+								className="newMessage-input"
+								value={this.state.inputText}
+								onChange={this.onInputChange}
+								onKeyPress={this.onEnterKeyPressed}
+								autoComplete="off"
+								spellCheck="false"
+								placeholder="Bochka is listening..."
+								required={true}
+							/>
 
-            <button className="auth-proceed" onClick={this.onAuth}>
-              PROCEED TO BOCHKA
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
+							<div className="newMessage-button" onClick={this.onNewMessageButtonClick}>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
+									<path d="M0 0h24v24H0z" fill="none" />
+								</svg>
+							</div>
+						</div>
+					</>
+				) : (
+					<div className="auth">
+						<h1>WHO THE FUCK ARE YOU?</h1>
+
+						<input
+							className="auth-input"
+							placeholder="I am..."
+							autoComplete="off"
+							autoCorrect="off"
+							spellCheck="false"
+							value={this.state.authorInputText}
+							onChange={this.onAuthInputChange}
+							required={true}
+						/>
+
+						<button className="auth-proceed" onClick={this.onAuth}>
+							PROCEED TO BOCHKA
+						</button>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
